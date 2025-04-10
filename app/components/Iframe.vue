10571664d57d202415cffc10542d14f8fead141a
fonzie <rudi@softmedia.nl>
2025-04-10 09:25:42 +0300

    Added iframe component Added nuxt image

---
+++ app/components/Iframe.vue
@@ -0,0 +1,54 @@
+<template>
+  <div
+    v-if="src"
+    class="iframe-container"
+    :style="{ 'padding-top': paddingTop }"
+  >
+    <iframe
+      :src="src"
+      frameborder="0"
+      scrolling="no"
+      tabindex="0"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const { src, aspectRatio } = defineProps({
+  src: {
+    type: String,
+    required: true
+  },
+  aspectRatio: {
+    type: String,
+    default: '1:2',
+    validator: v => !!v && /\d+:\d+$/.test(v.toString())
+  }
+})
+
+const paddingTop = computed(() => {
+  const [width, height] = aspectRatio.split(':').map(value => parseInt(value, 10))
+
+  return width && height ? `${(height / width) * 100}%` : '100%'
+})
+</script>
+
+<style scoped lang="scss">
+  .iframe-container {
+    position: relative;
+
+    width: 100%;
+    height: 100%;
+
+    > iframe {
+      height: 100%;
+      width: 100%;
+
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      left: 0;
+      top: 0;
+    }
+  }
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
+    '@nuxt/image'
   ],
   devtools: { enabled: true },
   future: {
